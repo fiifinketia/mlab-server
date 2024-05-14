2579389f44b8a3bd0cf54aacd2df7d589beb6007
Kelvin Nketia-Achiampong <achiampongk22@gmail.com>
2024-05-14 16:00:34 +0000

    feat:implement git server

---
+++ server/web/api/utils.py
@@ -0,0 +1,12 @@
+import os
+
+
+def make_git_path(name: str) -> str:
+    # use all lower case, replace any spaces with hyphens, and append ".git" to the name.
+    return name.lower().replace(" ", "-") + ".git"
+
+def create_git_project(filepath: str) -> None:
+    # Check if path to model exists
+    if os.path.exists(filepath):
+        raise Exception(f"Path {filepath} already exists")
+    os.system(f"git init --bare {filepath}")
