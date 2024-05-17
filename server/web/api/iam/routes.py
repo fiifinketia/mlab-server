187ebf8c36593d67a643e681b9601e7b8cf3e97c
Kelvin Nketia-Achiampong <achiampongk22@gmail.com>
2024-05-17 23:45:00 +0000

    feat: implement ssh key gen for git server connection

--- server/web/api/iam/routes.py
+++ server/web/api/iam/routes.py
@@ -1,5 +1,7 @@
 
 from fastapi import APIRouter, HTTPException
+import ormar
+import ormar.exceptions
 
 from server.db.models.iam import UserKeyPair
 from server.web.api.iam.utils import generate_key_pair, remove_key_pair
@@ -10,31 +12,33 @@ api_router = APIRouter()
 @api_router.post("/ssh_key")
 async def gen_key_pair(user_id: str) -> dict[str, bytes]:
     """Generate a new key pair for a user."""
-    old_key_pair = UserKeyPair.objects.get(user_id=user_id)
-    if old_key_pair:
-        await UserKeyPair.objects.delete(user_id=user_id)
-        remove_key_pair(user_id)
-    new_key_pair = generate_key_pair(user_id)
-    await UserKeyPair.objects.create(
-      user_id=user_id,
-      public_key=new_key_pair[0]
-    )
+    try:
+        old_key_pair = await UserKeyPair.objects.get(user_id=user_id)
+        if old_key_pair:
+            await UserKeyPair.objects.delete(user_id=user_id)
+            remove_key_pair(user_id)
+    finally:
+        new_key_pair = generate_key_pair(user_id)
+        await UserKeyPair.objects.create(
+          user_id=user_id,
+          public_key=new_key_pair[0]
+        )
 
-    key_pair = {
-      "public_key": new_key_pair[0],
-      "private_key": new_key_pair[1]
-    }
+        key_pair = {
+          "public_key": new_key_pair[0],
+          "private_key": new_key_pair[1]
+        }
 
     return key_pair
 
 @api_router.get("/ssh_key")
 async def get_key_pair(user_id: str) -> dict[str, bytes|str]:
     """Get the key pair for a user."""
-    key_pair = await UserKeyPair.objects.get(user_id=user_id)
-    if key_pair:
-        return {
+    try:
+      key_pair = await UserKeyPair.objects.get(user_id=user_id)
+      return {
           "public_key": key_pair.public_key,
           "private_key": "*********************************"
         }
-    else:
-        raise HTTPException(status_code=404, detail=f"Key pair for user {user_id} not found")
+    except ormar.exceptions.NoMatch:
+        raise HTTPException(status_code=404, detail=f"Key pair for user {user_id} not found")        
