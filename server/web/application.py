76c52d8eb05958a392cc2da36974b4af257e4adc
Kelvin Nketia-Achiampong <achiampongk22@gmail.com>
2024-06-03 20:46:00 +0000

    chore: Update check_auth middleware to handle Authorization header

--- server/web/application.py
+++ server/web/application.py
@@ -45,6 +45,7 @@ def get_app() -> FastAPI:
         if request.url.path.startswith("/api/docs") or request.url.path.startswith("/api/health") or request.url.path.startswith("/api/openapi.json"):
             return await call_next(request)
         credentials = request.headers.get("Authorization")
+        print(credentials)
         if credentials:
             if not credentials.startswith("Bearer "):
                 raise HTTPException(status_code=403, detail="Invalid authentication scheme.")
