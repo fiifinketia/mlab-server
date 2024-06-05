d108a560f37d64c730001d1253fd73d2756ee48e
Kelvin Nketia-Achiampong <achiampongk22@gmail.com>
2024-06-05 16:45:09 +0000

    chore: Update GitService clone_from method to remove unnecessary chmod command
    This commit modifies the `clone_from` method in the `GitService` class of the `main.py` file. It removes the unnecessary `chmod` command that was previously used to set permissions for the SSH key. The command is no longer needed as the key is now properly protected and accessible only by the owner. This change improves the efficiency and clarity of the codebase.

--- server/services/git/main.py
+++ server/services/git/main.py
@@ -56,7 +56,7 @@ class GitService:
         if self.check_exists(repo_name=repo_name_with_namspace):
             repo_git_url = self.make_clone_url(repo_with_namespace=repo_name_with_namspace)
             # allow all users to make changes to directory
-            Repo.clone_from(url=repo_git_url, to_path=to, branch=branch if branch is not None else "main", env={"GIT_SSH_COMMAND": f"ssh -i {settings.ssh_keys_path}/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"})
+            Repo.clone_from(url=repo_git_url, to_path=to, branch=branch if branch is not None else "main", env={"GIT_SSH_COMMAND": f"ssh -i {settings.ssh_keys_path}/id_rsa -o StrictHostKeyChecking=no"})
             return repo_git_url
         else:
             raise RepoNotFoundError(f"Repository '{repo_name_with_namspace}' does not exist.")
