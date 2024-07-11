265cb9cd29cf171de9510743191173de4496bfce
disal <disal@admin.git>
2024-07-11 00:40:36 +0000

    bug: SyntaxError: 'await' outside async function

--- server/web/api/jobs/service.py
+++ server/web/api/jobs/service.py
@@ -267,6 +267,7 @@ async def _setup_environment(
         create_task_request = runner_pb2.CreateTaskRequest(job_id=str(job_id), dataset=dataset, model=model)
         client = runner.client()
         client.create_task_environment(create_task_request)
+        print(create_task_request)
         job = await Job.objects.get(id=job_id)
         job.status = JobStatus.READY
         job.modified = datetime.datetime.now()
