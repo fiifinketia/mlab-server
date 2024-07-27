d3b374cad97246132784bd708a8ad350925bec94
disal <disal@admin.git>
2024-07-27 07:27:39 +0000

    chore: Update gRPC channel options in Runner class

--- server/services/balancer/runner.py
+++ server/services/balancer/runner.py
@@ -8,14 +8,16 @@ class Runner:
     def __init__(self, id: str, url: str,):
         self.url = url
         self.id = id
+        self.options = [('grpc.max_send_message_length', 1024 * 1024 * 1024), ('grpc.max_receive_message_length', 1024 * 1024 * 1024)]
 
     def client(self) -> runner_pb2_grpc.RunnerStub:
-        channel = grpc.insecure_channel(self.url)
+
+        channel = grpc.insecure_channel(self.url, options=self.options)
         stub = runner_pb2_grpc.RunnerStub(channel)
         return stub
 
     def client_aio(self) -> runner_pb2_grpc.RunnerStub:
-        channel = grpc.aio.insecure_channel(self.url)
+        channel = grpc.aio.insecure_channel(self.url, options=self.options)
         stub = runner_pb2_grpc.RunnerStub(channel)
         return stub
 
